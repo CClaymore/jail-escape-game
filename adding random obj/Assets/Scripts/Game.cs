8bb63647c4f3dd7a1ac228d5bba1026d138bfcc8
Xander <41644363+Baydock@users.noreply.github.com>
2022-11-27 20:16:30 -0600

    Revert "Fixed the sheriff's looking for the player, and removed the Game class"

---
+++ adding random obj/Assets/Scripts/Game.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+// The controller for all game logic
+public class Game : MonoBehaviour {
+    // The instance that other scripts can access
+    public static Game Instance { get; private set; }
+
+    // Start is called before the first frame update
+    void Start() {
+        // Set instance
+        Instance = this;
+
+        // Hide mouse and lock it
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        TryExit();
+    }
+
+    // Allow exit
+    void TryExit() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+    }
+}
